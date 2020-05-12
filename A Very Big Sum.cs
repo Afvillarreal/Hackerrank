682e97682c1a78ce5489b5ca6b635d0234a2859a
Andres Felipe Villarreal <51982396+Afvillarreal@users.noreply.github.com>
2020-05-11 22:49:11 -0500

    update Algorithms

---
+++ A Very Big Sum.cs
@@ -0,0 +1,10 @@
+﻿static long aVeryBigSum(long[] ar) {
+    
+        long total=0;
+        foreach (long num in ar){
+            total= total + num;           
+        }
+
+        Console.WriteLine(total);
+        return total;
+    }

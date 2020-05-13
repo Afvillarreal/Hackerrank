75c86e6b9a049ed7c3fee0df21fbacba03e80652
Andres Felipe Villarreal <51982396+Afvillarreal@users.noreply.github.com>
2020-05-13 18:20:15 -0500

    static void plusMinus(int[] arr) {

---
+++ Plus Minus.cs
@@ -0,0 +1,20 @@
+﻿static void plusMinus(int[] arr) {
+        
+        double zero = 0;
+        double positive = 0;
+        double negative = 0;
+
+        foreach (int num in arr){
+            
+            if (num == 0){
+                zero++;
+            }
+            if (num > 0 ) positive++;
+
+            if (num < 0) negative++; 
+        }
+        
+        Console.WriteLine(positive/arr.Count());
+        Console.WriteLine(negative/arr.Count());
+        Console.WriteLine(zero/arr.Count());
+    }

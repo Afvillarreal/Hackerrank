3e3d56374bb6e25ed8b1f8e28f289e1063d2bd28
Andres Felipe Villarreal <51982396+Afvillarreal@users.noreply.github.com>
2020-05-15 23:11:14 -0500

    Staircase

---
+++ Staircase.cs
@@ -0,0 +1,18 @@
+﻿static void staircase(int n) {
+        
+       for (int y = n-1; y>=0; y--)
+       {
+           
+           for (int x = 0; x<n; x++)
+           {
+               if (x>=y){
+                   Console.Write("#");
+               }
+               if (x<y){
+                   Console.Write(" ");
+               }
+           }
+           Console.WriteLine("");
+       }
+
+    }

c85a977ebe0ff410e324d7582422e5b3cd86c675
Andres Felipe Villarreal <51982396+Afvillarreal@users.noreply.github.com>
2020-05-15 22:54:25 -0500

    mini-max_sum

---
+++ mini-max sum.cs
@@ -0,0 +1,21 @@
+﻿ static void miniMaxSum(int[] arr) {
+         
+         int min = arr[0];
+         int max = arr[0];         
+         long sum = arr[0]; 
+
+        
+        for(int i=1;i<arr.Count();i++){
+
+            sum +=arr[i];
+
+            if (arr[i]<min){
+                min= arr[i];
+            }
+            if (arr[i]>max){
+                max= arr[i];
+            }
+        }
+        Console.WriteLine((sum -max)+" "+(sum - min));
+                  
+}

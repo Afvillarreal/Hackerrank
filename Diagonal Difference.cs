b3827987a1daf85c8c3c60b59ea445640d8e15ac
Andres Felipe Villarreal <51982396+Afvillarreal@users.noreply.github.com>
2020-05-17 18:15:47 -0500

    Diagonal Difference

---
+++ Diagonal Difference.cs
@@ -0,0 +1,15 @@
+﻿public static int diagonalDifference(List<List<int>> arr)
+    {
+        int sum = 0;
+        int rest = 0;
+
+        for (int i = 0; i<arr.Count();i++){
+                int index= arr.Count()-i-1;
+                Console.WriteLine(arr[i][index]);
+                sum = sum +arr[i][i];
+                rest = rest +arr[i][index];
+        }
+       int total = (sum-rest);       
+    
+       return Math.Abs(total);
+    }

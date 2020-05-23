fca3643608282fca1c8863d90952574407665a48
Andres Felipe Villarreal <51982396+Afvillarreal@users.noreply.github.com>
2020-05-23 18:34:29 -0500

    Birthday Cake Candles

---
+++ Birthday Cake Candles.cs
@@ -0,0 +1,16 @@
+﻿static int birthdayCakeCandles(int[] ar) {
+        int mayor = 0;
+        int count = 0;
+
+        foreach(var i in ar){
+            if (i>mayor){
+                count=0;
+                mayor = i;                
+            }
+            if (mayor==i){
+                count++;
+            }
+        }
+        return count;
+
+    }

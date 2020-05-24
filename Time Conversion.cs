0809696d85bf231de26a0846f587b41575bd425a
Andres Felipe Villarreal <51982396+Afvillarreal@users.noreply.github.com>
2020-05-23 21:56:37 -0500

    Time Conversion

---
+++ Time Conversion.cs
@@ -0,0 +1,49 @@
+﻿ static string timeConversion(string s) {
+        
+        string total= "";
+        bool pm = false;
+        bool am = false;
+
+        string start = s.Split(':')[0];
+        string middle = s.Split(':')[1];
+        string final = s.Split(':')[2];
+       
+        int hour = Convert.ToInt16(start);
+        int minute = Convert.ToInt16(middle);
+        
+        for (int i=0;i<s.Length;i++){
+            var moment = s.Substring(i,1);
+
+            if (moment == "P"){
+                pm=true;
+                break;
+            }
+            if (moment == "A"){
+                am=true;
+                break;
+            }                     
+        }
+
+        if (pm){
+            final= final.Split("PM".ToCharArray())[0];
+            if (hour != 12){
+                hour += 12;
+            }            
+            start = Convert.ToString(hour);
+            total =start+":"+middle+":"+final;
+            return total;
+        }
+
+        else{
+
+            if (hour==12){
+                start="00";
+            }
+
+            final= final.Split("AM".ToCharArray())[0];
+            total = start+":"+middle+":"+final;
+
+            return total;
+        }       
+        
+    }

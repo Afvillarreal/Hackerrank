ba796ae005ddcea3cc8793144bba24f0b84f5fa7
Andres Felipe Villarreal <51982396+Afvillarreal@users.noreply.github.com>
2020-06-10 23:47:55 -0500

    Grading Students

---
+++ Grading Students.cs
@@ -0,0 +1,44 @@
+﻿ public static List<int> gradingStudents(List<int> grades)
+        {
+            List<int> lista = new List<int>();
+            
+            foreach(var j in grades)
+            {
+                int count = 0;
+                int moment = j;
+
+                if (moment < 38 || moment % 5 == 0)
+                {
+                    lista.Add(moment);
+                }
+
+                else
+                {
+                    int copy = moment;
+
+                    while (count < 2)
+                    {
+                        moment++;
+                        count++;
+
+                        if (moment % 5 == 0)
+
+                        {
+                            lista.Add(moment);
+                            break;
+                        }
+
+                        if (count == 2)
+                    {
+                        lista.Add(copy);
+                    }
+                      
+                    }
+
+                                     
+                }
+            }
+
+            return lista;
+
+        }
